2bc8f07e5668074422fd5c953680fd57fad3f17d
unknown <mandenno228@gmail.com>
2021-08-06 12:13:05 +0300

    update

---
+++ natio.py
@@ -0,0 +1,13 @@
+#New Program on nationality
+
+print ('Hello New User')
+
+print (input('How are you doing'))
+
+nationality = input ('What continent are you from?')
+
+if nationality in('Africa', 'Europe', 'The American Region',):
+    print ('Oh lovely to hear, which country')
+else:
+    print(input ('How long have you been in that continent'))
+
